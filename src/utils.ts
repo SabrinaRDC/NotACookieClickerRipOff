84a95f5dc25bd9613e04dbda00d24e5e54c98759
SabrinaRDC <hethordc+github@gmail.com>
2021-06-04 02:43:31 +0800

    added more generators and upgrades, improved number readablity for higher numbers

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,6 @@
 export function convertNumber(cookie: number) {
+    if (cookie >= 1000000000000000) return String(Math.round(cookie / 10000000000) / 100 + 'Qua');
+    if (cookie >= 1000000000000) return String(Math.round(cookie / 10000000000) / 100 + 'Tri');
     if (cookie >= 1000000000) return String(Math.round(cookie / 10000000) / 100 + 'Bil');
     if (cookie >= 1000000) return String(Math.round(cookie / 10000) / 100 + 'Mil');
     if (cookie >= 1000) return String(Math.round(cookie / 10) / 100 + 'k');
