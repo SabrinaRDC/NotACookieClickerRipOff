94882f7ede293f7af68eff4d25e2a2d850aad4d5
Felix Fong <moongod101@hotmail.com>
2021-06-13 21:42:59 +0800

    Add back the reset game evnet dispatcher

--- src/ui/ResetGameButton.svelte
+++ src/ui/ResetGameButton.svelte
@@ -1,18 +1,21 @@
 <script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+    const eventDispatcher = createEventDispatcher();
     function resetGame() {
+        eventDispatcher('game.reset');
         localStorage.removeItem('game.player');
         localStorage.removeItem('game.shop');
-        location.reload();
+        eventDispatcher('game.reset.finish');
     }
 </script>
 
 <button on:click={resetGame}>Reset Game</button>
 
 <style>
- button {
-     position: fixed;
-     color: red;
-     left: 5px;
-     top: 92px;
- }
+    button {
+        position: fixed;
+        color: red;
+        left: 5px;
+        top: 92px;
+    }
 </style>
